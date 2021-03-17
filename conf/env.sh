bb16cda39bda5cd163358827ee43662bee12b7a3
Yijie Shen <henry.yijieshen@gmail.com>
2021-03-17 14:27:00 +0800

    Change max direct memory

--- conf/env.sh
+++ conf/env.sh
@@ -9,7 +9,7 @@ export SS_CONF_DIR="${SS_CONF_DIR:-"${SS_HOME}/conf"}"
 export LD_PRELOAD=/usr/lib64/libjemalloc.so
 export SS_GC_OPT=" -Dproc_spark_shuffle -Xmx1000m -server -XX:+UseG1GC -XX:MetaspaceSize=128m -XX:MaxGCPauseMillis=500 -XX:ParallelGCThreads=24 -XX:ConcGCThreads=6 -XX:+AggressiveOpts -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:-ResizePLAB -XX:+UseStringDeduplication -XX:+PrintAdaptiveSizePolicy -XX:InitiatingHeapOccupancyPercent=75 -XX:+UnlockExperimentalVMOptions -XX:G1HeapWastePercent=5 -XX:G1MixedGCLiveThresholdPercent=85 -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=1024M"
 SS_OPTS=$SS_GC_OPT" -Xloggc:/home/var/log/yarn/spark-shuffle-gc.log -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/home/var/lib/yarn/"
-SS_OPTS=$SS_OPTS" -Dio.netty.maxDirectMemory=5400000000"
+SS_OPTS=$SS_OPTS" -Dio.netty.maxDirectMemory=4400000000"
 export SS_OPTS=$SS_OPTS" -Dlog4j.configuration=file:${SS_HOME}/conf/log4j.properties"
 
 # some Java parameters
