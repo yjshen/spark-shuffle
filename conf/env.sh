6f41aa8d5db78264a8513ffc7d586feed34f6c78
Yijie Shen <henry.yijieshen@gmail.com>
2021-05-19 18:18:23 +0800

    rolling log file every 1024MB

--- conf/env.sh
+++ conf/env.sh
@@ -6,10 +6,23 @@ if [ -z "${SS_HOME}" ]; then
 fi
 export SS_CONF_DIR="${SS_CONF_DIR:-"${SS_HOME}/conf"}"
 
+if [ "$SS_IDENT_STRING" = "" ]; then
+  export SS_IDENT_STRING="$USER"
+fi
+
+export SS_LOG_DIR=/home/var/log/yarn/spark-shuffle/log/
+export SS_LOG_FILE="spark-shuffle-service-$SS_IDENT_STRING-$HOSTNAME.log"
+export SS_PID_DIR=/home/var/log/yarn/spark-shuffle/pid/
+export SS_ROOT_LOGGER="INFO,RFA"
+
 export LD_PRELOAD=/usr/lib64/libjemalloc.so
 export SS_GC_OPT=" -Dproc_spark_shuffle -Xmx1000m -server -XX:+UseG1GC -XX:MetaspaceSize=128m -XX:MaxGCPauseMillis=500 -XX:ParallelGCThreads=24 -XX:ConcGCThreads=6 -XX:+AggressiveOpts -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:-ResizePLAB -XX:+UseStringDeduplication -XX:+PrintAdaptiveSizePolicy -XX:InitiatingHeapOccupancyPercent=75 -XX:+UnlockExperimentalVMOptions -XX:G1HeapWastePercent=5 -XX:G1MixedGCLiveThresholdPercent=85 -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=1024M"
 SS_OPTS=$SS_GC_OPT" -Xloggc:/home/var/log/yarn/spark-shuffle-gc.log -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/home/var/lib/yarn/"
 SS_OPTS=$SS_OPTS" -Dio.netty.maxDirectMemory=4400000000"
+SS_OPTS="$SS_OPTS -Dss.log.dir=$SS_LOG_DIR"
+SS_OPTS="$SS_OPTS -Dss.log.file=$SS_LOG_FILE"
+SS_OPTS="$SS_OPTS -Dss.root.logger=${SS_ROOT_LOGGER:-INFO,console}"
+
 export SS_OPTS=$SS_OPTS" -Dlog4j.configuration=file:${SS_HOME}/conf/log4j.properties"
 export java_opts_array=($SS_OPTS -XX:OnOutOfMemoryError="kill -9 %p")
 
@@ -27,5 +40,5 @@ fi
 
 JAVA=$JAVA_HOME/bin/java
 
-SS_LOG_DIR=/home/var/log/yarn/spark-shuffle/log/
-SS_PID_DIR=/home/var/log/yarn/spark-shuffle/pid/
+export SS_LOG_DIR=/home/var/log/yarn/spark-shuffle/log/
+export SS_PID_DIR=/home/var/log/yarn/spark-shuffle/pid/
